ca4880170845d06b20fc58e3094a6bb892f81d6c
A H M SAIFUL ISLAM <iahmsaiful@gmail.com>
2023-12-11 23:48:03 +0100

    Login page initial commit

---
+++ HTML-CSS-JS/Login Page/script.js
@@ -0,0 +1,7 @@
+function login() {
+    // For simplicity, let's consider a successful login.
+    // You would typically perform authentication on the server.
+
+    // Redirect to the welcome page after successful login.
+    window.location.href = 'welcome.html';
+}
