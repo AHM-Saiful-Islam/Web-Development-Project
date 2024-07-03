69e6924275ee9eaed2e9a1f8470bd0ab80c2e625
A H M SAIFUL ISLAM <iahmsaiful@gmail.com>
2024-07-03 16:03:18 +0200

    Chat App Update
    two person can chat with each other on the same device.
    socket.io for real-time message delivery.
    get, post, http, body-parser, express.

--- NodeJs-Project/MyChat/server.js
+++ NodeJs-Project/MyChat/server.js
@@ -1,11 +1,34 @@
 var express = require("express")
-
+var bodyParser = require("body-parser")
+const { Socket } = require("socket.io")
 var app = express()
+var http = require('http').Server(app)
+var io = require('socket.io')(http)
 
 app.use(express.static(__dirname))
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended: false}))
 
+var messages = [
+    {name: "jane", message: "Hello"},
+    {name: "John", message: "Hi"}
+] 
 
-var server = app.listen(3010, () => {
+app.get('/messages', (req, res) => { // route and res and req
+    res.send(messages)
+})
+
+app.post('/messages', (req, res) => { // route and res and req
+    messages.push(req.body)
+    io.emit('message', req.body)
+    res.sendStatus(200)
+})
+
+io.on('connection', (Socket) => {
+    console.log(' user connected')
+})
+
+var server = http.listen(3010, () => {
 
     console.log("Server is listening on port ", server.address().port)
 
