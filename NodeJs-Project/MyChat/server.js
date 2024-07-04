abd61f50bb9cfbd78408d87f79bb2b64c72c2c64
A H M SAIFUL ISLAM <iahmsaiful@gmail.com>
2024-07-04 21:07:16 +0200

    Chat App with Node Update
    Basic chat app is completed.
    Real time chatting but from localhost.
    Response from MongoDB.
    Connected to DB.

--- NodeJs-Project/MyChat/server.js
+++ NodeJs-Project/MyChat/server.js
@@ -4,29 +4,59 @@ const { Socket } = require("socket.io")
 var app = express()
 var http = require('http').Server(app)
 var io = require('socket.io')(http)
+var mongoose = require('mongoose')
+const { error } = require("console")
+const { send } = require("process")
 
 app.use(express.static(__dirname))
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended: false}))
 
-var messages = [
-    {name: "jane", message: "Hello"},
-    {name: "John", message: "Hi"}
-] 
+var dbUrl = 'mongodb+srv://DBuserName:passCode@atlascluster.zirft53.mongodb.net/mychatdb'
 
-app.get('/messages', (req, res) => { // route and res and req
-    res.send(messages)
+// mongoose model (name, schema)
+var Message = mongoose.model('Message', {
+    name : String,
+    message: String
 })
 
+
+app.get('/messages', (req, res) => { // route and res and req
+    Message.find({})
+        .then(messages => {
+            res.send(messages);
+        })
+        .catch(err => {
+            res.status(500).send(err);
+        });
+})
+
+// mongoose is only work with promise
 app.post('/messages', (req, res) => { // route and res and req
-    messages.push(req.body)
-    io.emit('message', req.body)
-    res.sendStatus(200)
+    var message = new Message(req.body)
+    message.save()
+        .then(() => {
+            io.emit('message', req.body)
+            res.sendStatus(200)
+        })
+        .catch((err) => {
+            sendStatus(500)
+        })
 })
 
 io.on('connection', (Socket) => {
     console.log(' user connected')
 })
+
+// mongoose is only work with promise
+mongoose.connect(dbUrl)
+    .then(() => {
+        console.log('MongoDB connection is successful.')
+    })
+    .catch((err) => {
+        console.error('Failed to connect to MongoDB', err)
+    })
+
 
 var server = http.listen(3010, () => {
 
